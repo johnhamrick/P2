3eba4690725f3881703f9f33a33fdcd558663f4c
John Hamrick <john@hamrickjohn.me>
2014-10-02 21:54:29 -0400

    Edited $array

--- logic.php
+++ logic.php
@@ -3,9 +3,8 @@
 $manywords = $_POST['numWords'];
 $length = $_POST['maxlength'];
 
-$ARRAY GOES HERE = ['hello', 'goodbye', 'apple', 'banana', 'orange', 'table', 'chair', 'window', 'car', 'dog', 'cat', 'horse', 'forget', 'remember', 'number', 'letter', 'guitar', 'trumpet', 'trombone', 'kazoo'];
+$array = ['hello', 'goodbye', 'apple', 'banana', 'orange', 'table', 'chair', 'window', 'car', 'dog', 'cat', 'horse', 'forget', 'remember', 'number', 'letter', 'guitar', 'trumpet', 'trombone', 'kazoo'];
 
-//print("The number of words is $manywords and the max length is $length");
+print("The number of words is $manywords and the max length is $length");
 
-//print "The variables are $manywords and $length";
-
+print "The variables are $manywords and $length";
