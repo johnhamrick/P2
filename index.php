bceb6ee65b71fc3c7c9879def0efac2fcfbe8a4b
John Hamrick <john@hamrickjohn.me>
2014-10-04 00:34:37 -0400

    Further code cleanup

--- index.php
+++ index.php
@@ -4,27 +4,23 @@
 	
 	<meta charset='utf-8'>
 	<meta name='viewport' content='width=device-width, initial-scale=1'>
-	<link href='style.css' rel="stylesheet">
 	<title>Project 2 | John Hamrick | CSCI E-15</title>
+<?php require'logic.php'; ?>
 
-<?
-php require
-'logic.php';
-?>
-
+<link href='style.css' rel="stylesheet">
 </head>
 <body>
 	<div class='container'>
 		<h1>Password Generator</h1>
 		<h2>ala The xkcd Comic Strip</h2>
 
-		<p class='password'> <?php echo $password ?></p>
+	<p class='password'><?php echo $newPassword= "" ?></p>
 
 		<form method='POST' action='index.php'><br>
 			<p class='options'>
 
 				<label for='numWords'># of Words</label>
-				<input name='numWords' type='number' min='1' max='4' required='required'/>(Max 4)<br>
+				<input name='numWords' type='number' min='1' max='4' required='required' id = 'numWords'/>(Max 4)<br>
 				<br>
 				<input name='add_number' type='checkbox' id='add_number' value = '1'>
 				<label for='add_number'>Add a number</label><br>
@@ -50,7 +46,3 @@ php require
 
 </body>
 </html>
-
-
-
-
